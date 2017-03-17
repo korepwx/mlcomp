876447d44b5343383fa4f544954ef7ed1516fddf
Haowen Xu <public@korepwx.com>
2017-03-17 20:47:40 +0800

    minor update

---
+++ mlcomp-debug.py
@@ -0,0 +1,33 @@
+import os
+from logging import basicConfig
+
+import six
+
+from mlcomp.persist import Storage
+from mlcomp.persist.board import config
+from mlcomp.persist.board.application import MainApp
+from mlcomp.utils import TemporaryDirectory
+
+basicConfig(level='DEBUG')
+
+with TemporaryDirectory() as tempdir:
+    f = lambda s: (
+        s if isinstance(s, six.text_type) else s.decode('utf-8'))
+
+    # populate the storage trees
+    storage_dict = {}
+    for name in ['a/1', 'a/2', 'a/b', 'b/1', 'b/2', 'c']:
+        s = Storage(os.path.join(tempdir, name), mode='create')
+        s.description = name
+        storage_dict[name] = s
+
+    # construct the application
+    config['DEBUG'] = True
+    app = MainApp({
+        '/': os.path.join(tempdir, 'a'),
+        '/b/': os.path.join(tempdir, 'b'),
+        '/c/': os.path.join(tempdir, 'c'),
+    })
+
+    with storage_dict['a/1'].with_context():
+        app.run(debug=True, use_reloader=False, port=8888)
