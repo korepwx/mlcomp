410afe0fa6c82accc93fc598f0988b2b03050ae0
Haowen Xu <public@korepwx.com>
2017-03-20 20:49:24 +0800

    minor update

--- tests/utils/misc.py
+++ tests/utils/misc.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
-import numpy as np
+import os
+import unittest
 
-__all__ = ['big_number_verify']
+import numpy as np
+import six
+
+__all__ = ['big_number_verify', 'model_test']
 
 
 def big_number_verify(x, mean, stddev, n_samples, scale=4.):
@@ -9,3 +13,17 @@ def big_number_verify(x, mean, stddev, n_samples, scale=4.):
         np.abs(x - mean), stddev * scale / np.sqrt(n_samples),
         err_msg='away from expected mean by %s stddev' % scale
     )
+
+
+def model_testing_enabled():
+    flag = os.environ.get('MLCOMP_TEST_MODELS', '').lower()
+    return flag in ('1', 'on', 'yes', 'true')
+
+
+def model_test(method):
+    @six.wraps(method)
+    def inner(*args, **kwargs):
+        method = unittest.skipUnless(
+            model_testing_enabled(), 'Model testing is not enabled.')
+        return method(*args, **kwargs)
+    return inner
