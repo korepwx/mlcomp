dc4271cd0348f92ec91fcd4c0384c43e41b82e78
Haowen Xu <public@korepwx.com>
2017-04-06 10:53:53 +0800

    remove Keras related code

--- mlcomp/snippet/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Snippets of deep learning models.
-
-This packages contains pieces of code about various machine learning models.
-We hope these snippets would be useful prototype for more complex ones.
-"""
